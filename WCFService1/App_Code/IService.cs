f48d720300841b53cb11e6d6b8f6e07bf34a27b6
SLILAM Anwar <slilamanouar@gmail.com>
2020-12-13 15:30:35 +0100

    initial commit

---
+++ WCFService1/App_Code/IService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService" à la fois dans le code et le fichier de configuration.
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+	int WebAdd(int x, int y);
+	[OperationContract]
+	int WebSoustraction(int x, int y);
+	[OperationContract]
+	int WebMultiply(int x, int y);
+	[OperationContract]
+	int WebDivision(int x, int y);
+}
+
+
---
+++ WCFService1/App_Code/Service.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service" dans le code, le fichier svc et le fichier de configuration.
+public class Service : IService
+{
+
+	public int WebAdd(int x, int y)
+	{
+		return x + y;
+	}
+	public int WebSoustraction(int x, int y)
+	{
+		return x - y;
+	}
+	public int WebMultiply(int x, int y)
+	{
+		return x * y;
+	}
+	public int WebDivision(int x, int y)
+	{
+		return x / y;
+	}
+}
